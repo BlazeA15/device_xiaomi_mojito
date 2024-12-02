5ba269be01882cb286cfd9c9de169b582178762c
Sachinpawar86 <sachinbarange86@gmail.com>
2024-12-02 15:52:04 +0000

    mojito : bringup for infinity-x

--- infinity_mojito.mk
+++ infinity_mojito.mk
@@ -11,24 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some LineageOS stuffs
-$(call inherit-product, vendor/everest/config/common_full_phone.mk)
+# Inherit some Infinity-X stuffs
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
+INFINITY_MAINTAINER := Sachin_07
 
-# Maintainer name for Everest
-EVEREST_BUILD_TYPE := OFFICIAL
-EVEREST_MAINTAINER := K4LCHAKRA_&_ROMGHARTI
-
-# Build GAPPS\Vanilla
-WITH_GAPPS := true
-
-# Quick witch (add more than one Launcher in build)
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
+# PRODUCT_SYSTEM_PROPERTIES
+ro.infinity.soc=Snapdragon 678
+ro.infinity.battery=5000 mAh
+ro.infinity.display=1080 x 2400, 60 Hz
+ro.infinity.camera=48MP + 8MP + 2MP + 2MP
+ro.product.marketname=Redmi Note 10 4G
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := everest_mojito
+PRODUCT_NAME := infinity_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
